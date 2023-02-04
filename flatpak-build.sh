823db227c2fa74011f55e1b74f1ca3e147ab7173
SalaniLeo <97695689+SalaniLeo@users.noreply.github.com>
2023-02-04 18:58:13 +0100

    update

--- flatpak-build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-flatpak-builder --repo=dev.salaniLeo.Flake --force-clean build-dir dev.salaniLeo.flake.json
--- local build/flatpak-build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd ..
-flatpak-builder --repo='local build'/dev.salaniLeo.Flake --force-clean build-dir 'local build'/dev.salaniLeo.flake.json
